f6fc376448204183de7a24bd5b62a51f56527677
Laptop_ivanroj <ivro1@yandex.ru>
2025-03-19 11:58:15 +0300

    Update button /start 2

--- bot.py
+++ bot.py
@@ -27,7 +27,9 @@ async def send_welcome(message: Message):
     await message.answer("Привет! Для входа в приложение нажми на кнопку ниже:", reply_markup=keyboard)
 
 async def main():
+    # await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())

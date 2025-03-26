6b0432fa031b40b3241e2803605d13ae20e7dbbc
Laptop_ivanroj <ivro1@yandex.ru>
2025-03-26 10:17:11 +0300

    Update button /start and amvera.yml 1

--- TelegramApp/views.py
+++ TelegramApp/views.py
@@ -10,51 +10,7 @@ from asgiref.sync import async_to_sync
 # Импортируйте токен из настроек проекта
 from GameLauncher.settings import TELEGRAM_BOT_TOKEN
 
-# # Создаем приложение с помощью ApplicationBuilder (python-telegram-bot v20+)
-# application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
-#
-# logger = logging.getLogger(__name__)
-#
-# # Асинхронный обработчик команды /start
-# async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-#     # URL мини-приложения
-#     web_app_url = "https://telegram-app-richardyoung.amvera.io/"
-#
-#     # Создаем кнопку с объектом InlineKeyboardButton, передавая параметр web_app
-#     keyboard = [
-#         [
-#             InlineKeyboardButton(
-#                 text="Игровой лаунчер",
-#                 web_app={"url": web_app_url}
-#             )
-#         ]
-#     ]
-#     print('start/handler')
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#
-#     await update.message.reply_text(
-#         "Добро пожаловать! Запустите мини-приложение:",
-#         reply_markup=reply_markup
-#     )
-# # Регистрируем обработчик команды /start
-# application.add_handler(CommandHandler("start", start))
 
-
-# Синхронный view для обработки webhook-обновлений от Telegram
-# @csrf_exempt
-# def telegram_webhook(request):
-#     if request.method == "POST":
-#         try:
-#             data = json.loads(request.body.decode("utf-8"))
-#             logger.info(f"Получено обновление: {data}")  # Логируем входящие данные
-#             update = Update.de_json(data, application.bot)
-#         except Exception as e:
-#             logger.error(f"Ошибка декодирования: {e}")
-#             return HttpResponseBadRequest(f"Ошибка декодирования: {e}")
-#
-#         async_to_sync(application.process_update)(update)
-#         return JsonResponse({"status": "ok"})
-#     return HttpResponseBadRequest("Метод не поддерживается")
 
 def miniapp_home(request):
     # Список игр, которые будут отображаться на главном экране
